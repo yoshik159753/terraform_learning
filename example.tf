e2e93090e7f9e9be08f5a59795a07484e11bf227
Yoshi K <kai.outsider.777+git@gmail.com>
2020-05-30 06:03:45 +0900

    Provision / local-exec

--- example.tf
+++ example.tf
@@ -6,4 +6,8 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-003634241a8fcdec0"
   instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
