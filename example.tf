0be8502d627bc1e989cd9b86ce9d8be9671da10a
Yoshi K <kai.outsider.777+git@gmail.com>
2020-05-30 05:59:05 +0900

    Resource Dependencies
    s3 への依存と eip の設定。
    また依存なしのインスタンス作成。

--- example.tf
+++ example.tf
@@ -3,7 +3,31 @@ provider "aws" {
   region     = "us-west-2"
 }
 
+# New resource for the S3 bucket our application will use.
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "terraform-getting-started-guide-vUDjEixG"
+  acl    = "private"
+}
+
+# Change the aws_instance we declared earlier to now include "depends_on"
 resource "aws_instance" "example" {
   ami           = "ami-003634241a8fcdec0"
+  instance_type = "t2.micro"
+
+  # Tells Terraform that this EC2 instance must be created only after the
+  # S3 bucket has been created.
+  depends_on = [aws_s3_bucket.example]
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
+
+resource "aws_instance" "another" {
+  ami           = "ami-09dd2e08d601bff67"
   instance_type = "t2.micro"
 }
